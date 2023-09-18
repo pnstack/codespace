6be64e9856273612ba858efff8e4b50fe5572c66
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-09-19 00:08:36 +0700

    fix: install sdk

--- src/android-sdk/install.sh
+++ src/android-sdk/install.sh
@@ -1,20 +1,20 @@
 wget https://dl.google.com/android/repository/commandlinetools-linux-9123335_latest.zip
 unzip commandlinetools-linux-9123335_latest.zip
 mv cmdline-tools latest
-mkdir $HOME/.android
-mkdir $HOME/.android/cmdline-tools
-mv latest $HOME/.android/cmdline-tools
+mkdir /home/.android
+mkdir /home/.android/cmdline-tools
+mv latest /home/.android/cmdline-tools
 # Export the Android SDK path 
 rm -f ./commandlinetools-linux-9123335_latest.zip
 
-echo 'export ANDROID_HOME=$HOME/.android' >> ~/.bashrc
+echo 'export ANDROID_HOME=/home/.android' >> ~/.bashrc
 echo 'export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin' >> ~/.bashrc
 echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> ~/.bashrc
 echo 'export PATH=$PATH:$ANDROID_HOME/tools' >> ~/.bashrc
 echo 'export PATH=$PATH:$ANDROID_HOME/tools/bin' >> ~/.bashrc
 echo 'export PATH=$PATH:$ANDROID_HOME/emulator' >> ~/.bashrc
 
-yes | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "platform-tools" "platforms;android-33"
-yes | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "build-tools;33.0.0"
-yes | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "extras;android;m2repository"
-yes | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager --install "ndk;23.1.7779620"
+yes | /home/.android/cmdline-tools/latest/bin/sdkmanager "platform-tools" "platforms;android-33"
+yes | /home/.android/cmdline-tools/latest/bin/sdkmanager "build-tools;33.0.0"
+yes | /home/.android/cmdline-tools/latest/bin/sdkmanager "extras;android;m2repository"
+yes | /home/.android/cmdline-tools/latest/bin/sdkmanager --install "ndk;23.1.7779620"
